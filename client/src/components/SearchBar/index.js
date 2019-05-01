b3b9a7407674f562b8eb9958cb26b24aa648ac34
Alex Zimmerman <alex.zimmerman22209@gmail.com>
2019-05-01 14:35:19 -0600

    updated components

--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -3,7 +3,7 @@ import React from "react";
 // This component lets us use a bootstrap input element without worrying about class names
 // or manually wrapping the input with a form-group div
 // All of the props passed to this component are spread onto the input element
-function Input(props) {
+function SearchBar(props) {
     return (
         <div className="input-group input-group-lg">
             <input className="form-control" type="text" {...props} />
@@ -11,4 +11,4 @@ function Input(props) {
     );
 }
 
-export default Input;
+export default SearchBar;
