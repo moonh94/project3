68c50a51a7fc342daf82f306923a91a472123e65
robiwankenobi <rfnkurle@gmail.com>
2019-05-03 20:05:59 -0700

    Adding Register Form and Component

---
+++ client/src/components/RegisterForm/index.js
@@ -0,0 +1,42 @@
+
+
+import React from "react";
+import "./style.css";
+
+
+
+function RegisterForm(props) {
+
+    return (
+        <form>
+        <div className="form-group">
+            <label for="exampleInputEmail1">Name</label>
+            <input type="name" className="form-control" {...props}/>
+            
+        </div>
+        
+        <div className="form-group">
+            <label for="exampleInputPassword1">Position</label>
+            <input type="password" className="form-control" {...props}/>
+        </div>
+        
+        <div className="form-group">
+            <label for="exampleInputPassword1">Bio</label>
+            <input type="name" className="form-control" {...props}/>
+        </div>
+        <div className="form-group">
+            <label for="exampleInputPassword1">Location:</label>
+            <input type="name" className="form-control" {...props}/>
+        </div>
+        <div className="form-group">
+            <label for="exampleInputPassword1">Rate/hr</label>
+            <input type="name" className="form-control" {...props}/>
+        </div>
+        
+       
+        <button {...props} type="submit" className="btn btn-primary">{props.children}</button>
+        </form>
+
+    );
+}
+export default RegisterForm;
